d66d32a49064d2a1988943682fe9d2f4e4d1cb9d
Vladilav Yepifanov <evn1999@yandex.ru>
2019-04-24 16:05:25 +0300

    working hello world

--- src/main/java/com/example/tictactoe/Main.java
+++ src/main/java/com/example/tictactoe/Main.java
@@ -1,4 +1,22 @@
 package com.example.tictactoe;
 
-public class Main {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var button = new Button("Click me");
+        button.setOnAction(value -> Platform.exit());
+        var scene = new Scene(button, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
